35b63a807227f349d7ec75831d447979a933b784
gitmeyenhub <ozdemiremre42@hotmail.com>
2018-02-11 23:31:34 +0300

    Initial Commit

---
+++ INSTAGRAM FİREBASE/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  INSTAGRAM FİREBASE
+//
+//  Created by emre özdemir on 11.02.2018.
+//  Copyright © 2018 emre özdemir. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ INSTAGRAM FİREBASE/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  INSTAGRAM FİREBASE
+//
+//  Created by emre özdemir on 11.02.2018.
+//  Copyright © 2018 emre özdemir. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
